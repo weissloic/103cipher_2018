6c67647c8164dab57fbb816156f70989410224cb
benjamin <benjamin.kuhnel@epitech.eu>
2018-12-05 15:04:59 +0100

    first push

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** 103cypher
+** File description:
+** description
+*/
+
+#include "include/my.h"
+#include <string.h>
+#include <math.h>
+
+int main(int ac, char **av)
+{
+    return (0);
+}
