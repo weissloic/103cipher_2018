12912f698434764a1fdd1c031a549397716e33a6
benjamin <benjamin.kuhnel@epitech.eu>
2018-12-05 16:52:10 +0100

    key ascii

--- main.c
+++ main.c
@@ -5,11 +5,17 @@
 ** description
 */
 
-#include "include/my.h"
+#include "include/struct.h"
 #include <string.h>
 #include <math.h>
 
+void print_key_matrix(char *)
+{
+    
+}
+
 int main(int ac, char **av)
 {
+    print_key_matrix(av[2]);
     return (0);
 }
