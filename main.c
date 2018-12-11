f8cfcb398e6affd0005eff448707174352fa7256
benjamin <benjamin.kuhnel@epitech.eu>
2018-12-11 15:36:12 +0100

    add the message in matrix

--- main.c
+++ main.c
@@ -7,20 +7,38 @@
 
 #include "include/struct.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-void matrix_message(char *)
+void matrix_message(char *message, matrix_t *matrix)
 {
+    matrix->matrix = malloc(sizeof(int *) * matrix->nb_lines);
+    for (int i = 0; i < matrix->nb_lines; i++)
+        matrix->matrix[i] = malloc(sizeof(int) * matrix->nb_cols);
 
+    for (int i = 0; message[i] != '\0'; i++) {
+        for (int j = 0; j != matrix->nb_cols; j++) {
+            matrix->matrix[i][j] = message[i * matrix->nb_cols + j];
+        }
+    }
+    for (int i = 0; message[i] != '\0'; i++) {
+        for (int j = 0; j != matrix->nb_cols; j++)
+            printf("%d ", matrix->matrix[i][j]);
+        printf("\n");
+    }
 }
 
 int main(int ac, char **av)
 {
-    int nb_charac = strlen(av[1]);
-    printf("%d\n", nb_charac);
+    matrix_t *matrix_key = malloc(sizeof(matrix_t));
+    matrix_t *matrix_mess = malloc(sizeof(matrix_t));
 
-    char *stock_key = av[1];
-    for (int i = 0; stock_key[i] != '\0'; i++)
-        printf("%d\n", stock_key[i]);
-    return (0);
+    char *message = av[1];
+
+    matrix_mess->nb_cols = 3;
+    matrix_mess->nb_lines = strlen(message);
+    //printf("%d\n", matrix_mess->nb_lines);
+
+    matrix_message(message, matrix_mess);
 }
